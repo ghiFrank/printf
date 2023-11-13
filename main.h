f4d8477448f6f04f3986a2ef0d5da142af665387
Atyq96 <atyqamine1996@gmail.com>
2023-11-13 05:56:12 -0800

    fixing betty

--- main.h
+++ main.h
@@ -3,6 +3,11 @@
 
 
 int _printf(const char *format, ...);
+void print_char(char c, int *count);
+void print_str(const char *str, int *count);
+void handle_percent(const char *format, int *count);
+void handle_format_specifier(const char *format, va_list input, int *count);
+
 
 
 
