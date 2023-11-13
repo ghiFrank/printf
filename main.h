b2189a2a37c4875e4faa50ae97d683b261744f45
Atyq96 <atyqamine1996@gmail.com>
2023-11-13 06:11:45 -0800

    finally

--- main.h
+++ main.h
@@ -8,8 +8,4 @@ void print_str(const char *str, int *count);
 void handle_percent(const char *format, int *count);
 void handle_format_specifier(const char *format, va_list input, int *count);
 
-
-
-
-
 #endif
--- print_func.c
+++ print_func.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdrag.h>
+#include <stdarg.h>
 #include <unistd.h>
 
 /**
